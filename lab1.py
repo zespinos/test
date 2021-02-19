02629015861555c5e108aa87adc96475a9b2e2a8
zulema <zespinos@cisco.com>
2021-02-18 23:57:18 -0600

    Fin test e inici lab1 python

---
+++ lab1.py
@@ -0,0 +1,2 @@
+pi = 3.141592653589793
+print("pi is a with a value of ", pi)
--- test.py
+++ test.py
@@ -4,3 +4,25 @@ a = 5
 b = 15
 suma= a + b
 print ("La suma de ",a, "+", b, "es igual a ",suma)
+
+print("Hi, my  name is {}".format("Zulema"))
+
+print("Hi, my  name is {name}, and I am an {what}".format(name="Zulema", what="engineer"))
+
+print("a b cy z".split(" "))
+
+n=5
+if n < 5:
+    print("n is lees than five")
+elif n==5:
+    print("n is equal to five")
+else:
+    print("n is greater than five")
+
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+def say_hello():
+     print("Hello!")
