8ec7991e42393389d2dff49a25902b5c34470cf2
zulema <zespinos@cisco.com>
2021-02-19 00:13:32 -0600

    lab1 terminado

--- lab1.py
+++ lab1.py
@@ -1,2 +1,31 @@
 pi = 3.141592653589793
 print("pi is a with a value of ", pi)
+
+
+
+i =50
+if i < 5:
+    print("i is lees than", i)
+elif i==5:
+    print("i is equal to ", i)
+else:
+    print("i is greater than ",i)
+
+
+print("The fruit is {color}".format(color="orage"))
+print("    ")
+z=96
+multi = 12 * z
+print("12 x ",z, "=", multi)
+
+m=48
+c=17
+h= m*c
+print(m, "x", c, "=", h)
+
+
+def multiply(j,e):
+    result = j * e
+    return result
+
+print("12 x 96 = ", multiply(196523, 87323))
