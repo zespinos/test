5c8cd19669e13cdf2db10234f94338e215425ead
zulema <zespinos@cisco.com>
2021-02-19 16:11:42 -0600

    GUARDADO

--- bucles.py
+++ bucles.py
@@ -1,2 +1,15 @@
-for i in [1,2,3]:
-    print("Hola")
+contador=0
+miemail=input("Introduce tu direccion de email: ")
+
+for i in miemail:
+    if (i=="@"or i=="."):
+        contador=contador+1
+
+if contador >= 2:
+    print("Correcto")
+else:
+    print("incorrecto")
+
+
+for j in range(5):
+    print(j)
